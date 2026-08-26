4157e16c7a92b61c06759cfb065631d00659a83c
EduardoBrito9 <eduardobritodev@gmail.com>
2026-08-26 08:15:35 -0300

    OOP constructor

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    Student student1 = new Student("Eduardo", "ComputerScience", 19, "SWE at Google");
+}
---
+++ src/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+    String name;
+    String deegreCourse;
+    int age;
+    String dream;
+
+    Student(String name, String deegreCourse, int age, String dream){
+        this.name = name;
+        this.deegreCourse = deegreCourse;
+        this.age = age;
+        this.dream = dream;
+    }
+
+}
